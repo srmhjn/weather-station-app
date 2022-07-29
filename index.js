1f4e32dc57ec46933e374aec5102aed89bd5a57c
Shubham Mahajan <srmhjn@gmail.com>
2022-07-30 00:30:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const app = express();
 initRoutes(app);
 app.get('/',(req,res)=>res.send("<h1>WELCOME TO WEATHER STATION APP</h1>"));
 
-const server = app.listen(WEATHER_APP_PORT, () => console.log(`Listening on port ${WEATHER_APP_PORT}...`));
+const server = app.listen(process.env.PORT || WEATHER_APP_PORT, () => console.log(`Listening on port ${WEATHER_APP_PORT}...`));
 export default server;
