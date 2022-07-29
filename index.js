e021136fe8ef43f140f5e90cbc859abdd1dee14a
Shubham Mahajan <srmhjn@gmail.com>
2022-07-30 00:12:45 +0530

    Added empty route to handle home page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { initRoutes } from './startup/routes.js';
 
 const app = express();
 initRoutes(app);
+app.get('/',(req,res)=>res.send("<h1>WELCOME TO WEATHER STATION APP</h1>"));
 
 const server = app.listen(WEATHER_APP_PORT, () => console.log(`Listening on port ${WEATHER_APP_PORT}...`));
 export default server;
