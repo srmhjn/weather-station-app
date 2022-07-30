2fa081fe279c68666233e973163091f83585df7c
Shubham Mahajan <srmhjn@gmail.com>
2022-07-30 18:09:46 +0530

    Updated config path

--- test/mocks/serviceMock.js
+++ test/mocks/serviceMock.js
@@ -1,4 +1,4 @@
-import { messages } from "../../../../resources/config";
+import { messages } from "../../resources/config";
 
 export const getFilteredStationsServiceMock = async function() {
 	return {
